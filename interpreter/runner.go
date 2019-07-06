92956aa2ef292c1e30c9ad06284cf583c791c71f
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-07-06 22:17:57 +0100

    Remove redundant code

--- interpreter/runner.go
+++ interpreter/runner.go
@@ -4,7 +4,6 @@ package interpreter
 import (
 	"fmt"
 	"os"
-	"time"
 	"strings"
 	"regexp"
 )
@@ -32,10 +31,14 @@ func Execute(scroll *Scroll, scrollArgs []string) (exitCode int, err error) {
 	scroll.JumpToLine(1) // Ignore first line
 	
 	for scroll.NextCodeLine() {
+	
+		// TODO: Create a file and func that handles lines of ordinary scroll
+		// TODO: code, i.e. those at the root of a scroll, spell or in a 'for' block but not
+		// TODO: those within a 'when' block.
 		
 		// Note: The assignment operator '=' must have whitespace on both sides
 		// of it. It is only used for assignment, therefore, if the operator
-		// exists in a line it must be assigning something to a variable.
+		// exists in a statement it must be assigning something to a variable.
 		isAssignment, err := regexp.MatchString(`\s=\s`, scroll.Code)
 		if err != nil {
 			compilerBug(scroll, "bad regex used for finding assignment operator")
@@ -76,82 +79,4 @@ func compilerBug(scroll *Scroll, msg string) {
 	fmt.Println(info)
 	fmt.Print("\t..." + msg)
 	os.Exit(1)
-}
-
-/******************************************************************************
-	github.com/PaulioRandall/go-cookies/cookies
-******************************************************************************/
-/*
-// TrimWhitespace removes all white space from a string.
-func StripWhitespace(s string) string {
-	var buf bytes.Buffer
-	for _, r := range s {
-		if !unicode.IsSpace(r) {
-			buf.WriteRune(r)
-		}
-	}
-	return buf.String()
-}
-*/
-
-// StopWatch represents a process timer with a few common operations.
-type StopWatch struct {
-	Started time.Time
-	Stopped time.Time
-}
-
-// Start starts the stop watch overwritting any previously start time.
-func (sw *StopWatch) Start() {
-	sw.Started = time.Now().UTC()
-}
-
-// Stop stops the stop watch overwriting any previous stop time.
-func (sw *StopWatch) Stop() {
-	sw.Stopped = time.Now().UTC()
-}
-
-// Lap restarts the stop watch but returns a copy of the stop watch with the
-// previous laps times.
-func (sw *StopWatch) Lap() StopWatch {
-	sw.Stop()
-	r := StopWatch{
-		Started: sw.Started,
-		Stopped: sw.Stopped,
-	}
-	sw.Started = sw.Stopped
-	return r
-}
-
-// Elapsed returns the elapsed time between the started and stopped times.
-func (sw *StopWatch) Elapsed() time.Duration {
-	return sw.Stopped.Sub(sw.Started)
-}
-
-// PrintElapsed prints the elapsed time as returned by Elapsed(). 'radix' may be
-// passed to print the result in a more appropriate manner.
-//
-// e.g. PrintElapsed(time.Millisecond) will print as milliseconds.
-//
-// e.g. PrintElapsed(10 * 1000 * 1000) will print using a custom radix,
-// microseconds * 10 in this case.
-func (sw *StopWatch) PrintElapsed(radix time.Duration) {
-	t := sw.Elapsed()
-	f := float64(t) / float64(radix)
-
-	switch radix {
-	case time.Nanosecond:
-		fmt.Printf("%.3f ns\n", f)
-	case time.Microsecond:
-		fmt.Printf("%.3f us\n", f)
-	case time.Millisecond:
-		fmt.Printf("%.3f ms\n", f)
-	case time.Second:
-		fmt.Printf("%.3f s\n", f)
-	case time.Minute:
-		fmt.Printf("%.3f m\n", f)
-	case time.Hour:
-		fmt.Printf("%.3f hr\n", f)
-	default:
-		fmt.Printf("%.3f\n", f)
-	}
 }
