84183b9fc28db56a9feb4b9e8e2816c1be0e7b7c
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-07-17 22:13:14 +0100

    Add LexError.Line() so line number can be added later

--- lexer/lex_error.go
+++ lexer/lex_error.go
@@ -5,21 +5,31 @@ package lexer
 // and column number.
 type LexError interface {
 	error
+
+	// Line sets the line number.
+	Line(lineNum int) LexError
 }
 
 // stdLexError is the standard implementation of LexError.
 type stdLexError struct {
-	Err  string // Error message
-	Line int    // Line number
-	Col  int    // Column number
+	err  string // Error message
+	line int    // Line number
+	col  int    // Column number
 }
 
 // NewLexError returns a new initialised LexError.
 func NewLexError(err string, line, col int) LexError {
-	return LexError(stdLexError{err, line, col})
+	e := stdLexError{err, line, col}
+	return LexError(e)
 }
 
 // Error satisfies the error interface.
 func (l stdLexError) Error() string {
-	return l.Err
+	return l.err
+}
+
+// Line satisfies the LexError interface.
+func (l stdLexError) Line(lineNum int) LexError {
+	l.line = lineNum
+	return LexError(l)
 }
