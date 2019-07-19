727f62c5cbcf4e4c6b6c315ca31044c98dbae96a
PaulioRandall <PaulioRandall@noreply.github.com>
2019-07-19 16:12:22 +0100

    Remove SymItr funcs + create SymItr.Length() + tests

--- symbol/sym_itr.go
+++ symbol/sym_itr.go
@@ -1,54 +1,22 @@
 package symbol
 
-
 // SymItr provides a way to iterate symbol arrays.
 type SymItr struct {
 	index  int
 	length int
-	syms   []Symbol
+	array  []Symbol
 }
 
 // NewSymItr creates a new symbol iterator.
-func NewSymItr(syms []Symbol) *SymItr {
+func NewSymItr(s []Symbol) *SymItr {
 	return &SymItr{
-		length: len(syms),
-		syms:   syms,
+		length: len(s),
+		array:   s,
 	}
 }
 
-// HasNext returns true if there are items still to be iterated.
-func (itr *SymItr) HasNext() bool {
-	i := itr.index + 1
-	if i < itr.length {
-		return true
-	}
-	return false
+// Length returns the total length of the iterators array.
+func (itr *SymItr) Length() int {
+	return itr.length
 }
 
-// increment increments the index counter.
-func (itr *SymItr) increment() {
-	itr.index = itr.index + 1
-}
-
-// Skip the next symbol in the array by incrementing the
-// iterator index without returning anything.
-func (itr *SymItr) Skip() {
-	itr.increment()
-}
-
-// Next returns the next symbol in the array and increases the
-// iterator index. Nil is returned if no symbols remain.
-func (itr *SymItr) Next() *Symbol {
-	defer itr.increment()
-	return itr.Peek()
-}
-
-// Peek returns the next symbol in the array without
-// incrementing the iterator index.
-func (itr *SymItr) Peek() *Symbol {
-	if itr.HasNext() {
-		i := itr.index + 1
-		return &itr.syms[i]
-	}
-	return nil
-}
--- symbol/sym_itr_test.go
+++ symbol/sym_itr_test.go
@@ -1,1 +1,25 @@
 package symbol
+
+import (
+	"testing"
+	
+	"github.com/stretchr/testify/assert"
+)
+
+func dummySymArray(s ...string) []Symbol {
+	r := []Symbol{}
+	for _, v := range s {
+		sym := Symbol{
+			Val: v,
+		}
+		r = append(r, sym)
+	}
+	return r
+}
+
+func TestSymItr_Length(t *testing.T) {
+	s := dummySymArray()
+	itr := NewSymItr(s)
+	exp := len(s)
+	assert.Equal(t, exp, itr.Length())
+}
