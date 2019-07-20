03eb2a7b6a9d1bee9f41a5b1a7891006b408ded0
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-07-20 12:04:50 +0100

    Create strimmer test but disable it until strimmer starts

--- strimmer/strim_error.go
+++ strimmer/strim_error.go
@@ -4,6 +4,8 @@ import (
   "github.com/PaulioRandall/voodoo-go/lexer"
 )
 
+// TODO: Remove dependency on lexer
+
 // StrimError represents an error returned by the strimmer
 // and contains additional error information such as the
 // line and column number.
--- strimmer/strimmer_test.go
+++ strimmer/strimmer_test.go
@@ -1,1 +1,75 @@
 package strimmer
+
+import (
+  "testing"
+  "strconv"
+
+  "github.com/PaulioRandall/voodoo-go/lexeme"
+  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStrim(t *testing.T) {
+	for i, tc := range strimTests() {
+		t.Log("Strim() test case: " + strconv.Itoa(i+1))
+		ts, err := Strim(tc.Input)
+
+		if tc.ExpectErr != nil {
+			require.Nil(t, ts)
+			assert.NotEmpty(t, err.Error())
+			assert.Equal(t, tc.ExpectErr.Line(), err.Line())
+			assert.Equal(t, tc.ExpectErr.Col(), err.Col())
+		}
+
+		if tc.ExpectToks != nil {
+			require.Nil(t, err)
+			assert.Equal(t, tc.ExpectToks, ts)
+		}
+	}
+}
+
+type strimTest struct {
+	Input      []lexeme.Lexeme
+	ExpectToks []Token
+	ExpectErr  StrimError
+}
+
+type expStrimError struct {
+	line int // Line number
+	col  int // Column number
+}
+
+func (e expStrimError) Error() string {
+	// Error messages should be semantically validated
+	// so this is not required for testing.
+	return ""
+}
+
+func (e expStrimError) Line() int {
+	return e.line
+}
+
+func (e expStrimError) Col() int {
+	return e.col
+}
+
+func strimTests() []strimTest {
+	return []strimTest{
+		strimTest{
+			Input: []lexeme.Lexeme{
+        lexeme.Lexeme{`x`, 0, 1, 0, lexeme.IDENTIFIER},
+				lexeme.Lexeme{` `, 1, 2, 0, lexeme.WHITESPACE},
+				lexeme.Lexeme{`<-`, 2, 4, 0, lexeme.ASSIGNMENT},
+				lexeme.Lexeme{` `, 4, 5, 0, lexeme.WHITESPACE},
+				lexeme.Lexeme{`1`, 5, 6, 0, lexeme.NUMBER},
+      },
+/*
+      ExpectToks: []Token{
+        Token{`x`, 0, 1, 0, lexeme.IDENTIFIER},
+				Token{`<-`, 2, 4, 0, lexeme.ASSIGNMENT},
+				Token{`1`, 5, 6, 0, lexeme.NUMBER},
+      },
+*/
+		},
+  }
+}
