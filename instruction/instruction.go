76f3ce06f993d2df50750ceea1a3cb1028e438e1
PaulioRandall <PaulioRandall@noreply.github.com>
2019-07-22 16:31:18 +0100

    Finish detailing intended activities of the syntax analyser

--- instruction/instruction.go
+++ instruction/instruction.go
@@ -1,9 +1,14 @@
 
 package instruction
 
-// Instruction is a unit of activity. Instructions are
-// executed within a given context; usually, it will make
-// a single change to the specified context.
+// Instruction is a unit of activity within this
+// implementation. Instructions are executed within a given
+// context. Usually, it will make a single change to the
+// specified context. Instructions are considered atomic
+// like CPU instructions but not as fine grained since
+// activities such as fetching and placing values from and
+// to memory, or rather their entry in the context, are
+// considered part of the instruction.
 type Instruction interface {
 
 	// Exe executes the instruction within the given context
--- syntax/analyser.go
+++ syntax/analyser.go
@@ -10,17 +10,65 @@ import (
 // 1: Scanning the token array for precedence brackets, the
 //    the curvy ones, and splitting the token array into
 //    smaller ones until no brackets are left, an error is
-//    returned if each brace doesn't have an opposing
-//    partner. E.g...
-//    1st:
-//      #0: `x <- (a + b - (c * d))` - 13 tokesn
-//    2nd:
-//      #0: `x <- #1` - 3 tokens
-//      #1: `a + b - (c * d)` - 9 tokens
-//    3rd:
-//      #0: `x <- #1` - 3 tokens
-//      #1: `a + b - #2` - 5 tokens
-//      #2: `c * d` - 3 tokens
+//    returned if a brace doesn't have an opposing partner.
+//    Braces are removed along the way with temporary variables
+//    being created and inserted where needed.
+//    E.g...
+//    Input:
+//      #0 = [x, <-, (, a, +, 12.3, -, (, c, *, d, ), )] = 13 tokens
+//    Turns into:
+//      #0 = [x, <-, #1]
+//      #1 = [a, +, 12.3, -, (, c, *, d, )]
+//    Turns into:
+//      #0 = [x, <-, #1]
+//      #1 = [a, +, 12.3, -, #2]
+//      #2 = [c, *, d]
+//    Ordering is reversed:
+//      [#2, #1, #0]
+// 2: Each token array is then split into even smaller ones such
+//    that each contains exactly one instruction, error checking
+//    where possible.
+//    E.g... 
+//    Input:
+//      #2 = [c, *, d]
+//      #1 = [a, +, 12.3, -, #2]
+//      #0 = [x, <-, #1]
+//    Turns into:
+//      #2.1 = [c, *, d]
+//      #1.2 = [a, +, 12.3]
+//      #1.1 = [#1.2, -, #2.1]
+//      #0 = [x, <-, #1.1]
+// 3: Finally each token array is converted into its
+//    instruction counter part. Number and boolean literals
+//    are converted into Go float64 and bools accordingly.
+//    E.g...
+//    Input:
+//      #2.1 = [c, *, d]
+//      #1.2 = [a, +, 12.3]
+//      #1.1 = [#1.2, -, #2.1]
+//      #0 = [x, <-, #1.1]
+//    Turns into:
+//      InstructionSet{
+//        Multiply{
+//          Left: c,
+//          Right: d,
+//          As: #2.1,
+//        },
+//        Add{
+//          Left: a,
+//          Right: 12.3,
+//          As: #1.2,        
+//        },
+//        Subtract{
+//          Left: #1.2,
+//          Right: #2.1,
+//          As: #1.1,        
+//        },
+//        Assign{
+//          Val: #2.1,
+//          As: x,        
+//        },
+//      }
 func Analyse(ts []symbol.Token) (instruct.InstructionSet, instruct.InsError) {
 	return nil, nil
 }
