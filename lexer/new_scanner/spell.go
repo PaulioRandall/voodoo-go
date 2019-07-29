a69164f8b1bb69d2457118811f550dbbd954d32f
PaulioRandall <PaulioRandall@noreply.github.com>
2019-07-29 13:01:48 +0100

    Reimpl string scanning within new_scanner pkg + tests

--- lexer/new_scanner/spell.go
+++ lexer/new_scanner/spell.go
@@ -7,6 +7,8 @@ import (
 
 // scanSpell scans symbols that start with a the '@' rune
 // returning a spell identifier. Spells are inbuilt functions.
+//
+// This function asumes the first rune of the input array is a '@'.
 func scanSpell(in []rune) (tk *token.Token, out []rune, err fault.Fault) {
 
 	if len(in) < 2 || !isLetter(in[1]) {
---
+++ lexer/new_scanner/string_test.go
@@ -0,0 +1,72 @@
+package scanner
+
+import (
+	"testing"
+
+	"github.com/PaulioRandall/voodoo-go/fault"
+	"github.com/PaulioRandall/voodoo-go/token"
+)
+
+func TestScanString(t *testing.T) {
+	runFailableScanTest(t, "string_test.go", scanString, scanStringTests())
+}
+
+func scanStringTests() []scanFuncTest {
+	return []scanFuncTest{
+		scanFuncTest{
+			TestLine: fault.CurrLine(),
+			Input:    []rune(`""`),
+			Output:   []rune{},
+			Expect: token.Token{
+				Val:  `""`,
+				Type: token.LITERAL_STRING,
+			},
+		},
+		scanFuncTest{
+			TestLine: fault.CurrLine(),
+			Input:    []rune(`"From hell with love"`),
+			Output:   []rune{},
+			Expect: token.Token{
+				Val:  `"From hell with love"`,
+				Type: token.LITERAL_STRING,
+			},
+		},
+		scanFuncTest{
+			TestLine: fault.CurrLine(),
+			Input:    []rune(`"From hell with love", 123.456`),
+			Output:   []rune(`, 123.456`),
+			Expect: token.Token{
+				Val:  `"From hell with love"`,
+				Type: token.LITERAL_STRING,
+			},
+		},
+		scanFuncTest{
+			TestLine: fault.CurrLine(),
+			Input:    []rune(`"Simon: \"Leaders eat last!\""`),
+			Output:   []rune{},
+			Expect: token.Token{
+				Val:  `"Simon: \"Leaders eat last!\""`,
+				Type: token.LITERAL_STRING,
+			},
+		},
+		scanFuncTest{
+			TestLine: fault.CurrLine(),
+			Input:    []rune(`"\\\\\""`),
+			Output:   []rune{},
+			Expect: token.Token{
+				Val:  `"\\\\\""`,
+				Type: token.LITERAL_STRING,
+			},
+		},
+		scanFuncTest{
+			TestLine:  fault.CurrLine(),
+			Input:     []rune(`"`),
+			ExpectErr: fault.Dummy(fault.String),
+		},
+		scanFuncTest{
+			TestLine:  fault.CurrLine(),
+			Input:     []rune(`"escaped \"`),
+			ExpectErr: fault.Dummy(fault.String),
+		},
+	}
+}
