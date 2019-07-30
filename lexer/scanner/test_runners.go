0164cb6542bc68d5c74da648b1eb62b7182d7067
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-07-31 00:15:59 +0100

    Remove obsolete test runner

--- lexer/scanner/test_runners.go
+++ lexer/scanner/test_runners.go
@@ -4,7 +4,6 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/PaulioRandall/voodoo-go/fault"
 	"github.com/PaulioRandall/voodoo-go/new_fault"
 	"github.com/PaulioRandall/voodoo-go/token"
 	"github.com/stretchr/testify/assert"
@@ -18,7 +17,6 @@ type scanFuncTest struct {
 	Input        []rune
 	Output       []rune
 	Expect       token.Token
-	ExpectErr    fault.Fault
 	ExpectNewErr new_fault.Fault
 }
 
@@ -38,7 +36,7 @@ func runScanTest(
 
 	for _, tc := range tests {
 		require.NotNil(t, tc.Expect)
-		require.Nil(t, tc.ExpectErr)
+		require.Nil(t, tc.ExpectNewErr)
 
 		testLine := strconv.Itoa(tc.TestLine)
 		t.Log("-> " + fileName + " : " + testLine)
@@ -48,36 +46,6 @@ func runScanTest(
 		require.NotNil(t, tk, "Did not expect token to be nil")
 		assert.Equal(t, tc.Output, out, "Expected a different array of leftover runes")
 		assert.Equal(t, tc.Expect, *tk, "Expected a different token")
-	}
-}
-
-// runFailableScanTest runs the input test cases on the
-// input function.
-func runFailableScanTest(
-	t *testing.T,
-	fileName string,
-	f func([]rune) (*token.Token, []rune, fault.Fault),
-	tests []scanFuncTest) {
-
-	for _, tc := range tests {
-
-		testLine := strconv.Itoa(tc.TestLine)
-		t.Log("-> " + fileName + " : " + testLine)
-
-		tk, out, err := f(tc.Input)
-
-		if tc.ExpectErr != nil {
-			assert.Nil(t, tk, "Expected token to be nil")
-			require.NotNil(t, err, "Did NOT expect error to be nil")
-			assert.NotEmpty(t, err.Error(), "Did NOT expect error message to be empty")
-			fault.Assert(t, tc.ExpectErr, err)
-
-		} else {
-			assert.Nil(t, err, "Expected error to be nil")
-			require.NotNil(t, tk, "Did NOT expect token to be nil")
-			assert.Equal(t, tc.Output, out, "Expected a different array of leftover runes")
-			assert.Equal(t, tc.Expect, *tk, "Expected a different token")
-		}
 	}
 }
 
