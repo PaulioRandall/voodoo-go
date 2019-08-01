885fda49785bc492e8ef613d6b61947ef23c4d7f
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-08-01 11:12:15 +0100

    Create Parse() test case

--- parser/parser.go
+++ parser/parser.go
@@ -1,12 +1,13 @@
 package parser
 
 import (
+	"github.com/PaulioRandall/voodoo-go/expr/ctx"
 	"github.com/PaulioRandall/voodoo-go/fault"
 	"github.com/PaulioRandall/voodoo-go/token"
 )
 
 // Parse parses the input into a parse tree.
-func Parse(in []token.Token) (out *ParseTree, err fault.Fault) {
+func Parse(in []token.Token) (out ctx.Expression, err fault.Fault) {
 
 	return nil, nil
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -4,7 +4,8 @@ import (
 	"strconv"
 	"testing"
 
-	//"github.com/PaulioRandall/voodoo-go/expr/ctx"
+	"github.com/PaulioRandall/voodoo-go/expr/ctx"
+	"github.com/PaulioRandall/voodoo-go/expr/exprs"
 	"github.com/PaulioRandall/voodoo-go/fault"
 	"github.com/PaulioRandall/voodoo-go/token"
 	"github.com/stretchr/testify/assert"
@@ -14,8 +15,29 @@ import (
 type parseTest struct {
 	TestLine int
 	Input    []token.Token
-	Expect   ParseTree
+	Expect   ctx.Expression
 	Error    fault.Fault
+}
+
+type dummy struct {
+	Val ctx.Value
+	Err fault.Fault
+}
+
+func (d dummy) Evaluate(c *ctx.Context) (v ctx.Value, err fault.Fault) {
+	return d.Val, d.Err
+}
+
+func valDummy(v ctx.Value) ctx.Expression {
+	return dummy{
+		Val: v,
+	}
+}
+
+func errDummy(err fault.Fault) ctx.Expression {
+	return dummy{
+		Err: err,
+	}
 }
 
 func TestParser(t *testing.T) {
@@ -24,18 +46,18 @@ func TestParser(t *testing.T) {
 		testLine := strconv.Itoa(tc.TestLine)
 		t.Log("-> parser_test.go : " + testLine)
 
-		var tree *ParseTree
+		var act ctx.Expression
 		var err fault.Fault
-		tree, err = Parse(tc.Input)
+		act, err = Parse(tc.Input)
 
 		if tc.Error != nil {
-			assert.Nil(t, tree)
+			assert.Nil(t, act)
 			require.NotNil(t, err)
 
 		} else {
 			assert.Nil(t, err)
-			require.NotNil(t, tree)
-			assert.Equal(t, tc.Expect, tree)
+			require.NotNil(t, act)
+			assert.Equal(t, tc.Expect, act)
 		}
 
 	}
@@ -45,8 +67,7 @@ func makeParseTests() []parseTest {
 	return []parseTest{}
 }
 
-/*
-func makeParseTests() []parseTest {
+func NEW_makeParseTests() []parseTest {
 	return []parseTest{
 		parseTest{
 			TestLine: fault.CurrLine(),
@@ -55,7 +76,11 @@ func makeParseTests() []parseTest {
 				token.Token{`<-`, 2, 4, token.ASSIGNMENT},
 				token.Token{`1`, 5, 6, token.LITERAL_NUMBER},
 			},
+			Expect: exprs.Assignment{
+				Identifier: token.Token{`x`, 0, 1, token.IDENTIFIER_EXPLICIT},
+				Operator:   token.Token{`<-`, 2, 4, token.ASSIGNMENT},
+				Right:      valDummy(ctx.NumberValue(1)),
+			},
 		},
 	}
 }
-*/
