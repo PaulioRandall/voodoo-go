0cd9bfcecdb6b32e0be244d3feb29431b25635eb
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-08-04 11:16:59 +0100

    Reimpl assignment expression in it's own file

---
+++ exprs/assignment.go
@@ -0,0 +1,27 @@
+package exprs
+
+import (
+	"github.com/PaulioRandall/voodoo-go/ctx"
+	"github.com/PaulioRandall/voodoo-go/fault"
+	"github.com/PaulioRandall/voodoo-go/token"
+)
+
+// Assignment represents an assignment of values from the result
+// of each value of a set expressions to a set of identifiers.
+// Left and right set lengths must match otherwise invalid syntax.
+type Assignment struct {
+	Left     ctx.Expression // List of all identifiers on left side
+	Operator token.Token
+	Right    ctx.Expression // List created from joining results of all comma
+	// separated right side expressions
+}
+
+// StatName satisfies the Statement interface.
+func (a Assignment) StatName() string {
+	return `assignment`
+}
+
+// Evaluate satisfies the Express interface.
+func Evaluate(c *ctx.Context) fault.Fault {
+	return nil
+}
