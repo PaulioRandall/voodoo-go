ef178d2d7cbe9b3962ca4018bfd1b6e0d514c3af
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-08-04 20:31:30 +0100

    Refactor number sscanning for clarity

--- lexer/scanner/number.go
+++ lexer/scanner/number.go
@@ -15,24 +15,10 @@ func scanNumber(in []rune, col int) (tk *token.Token, out []rune, err fault.Faul
 	num, in = scanInt(in)
 	frac, out = scanFrac(in)
 
-	size := len(frac)
-
-	if size > 0 {
-		if size < 2 {
-			out = nil
-			err = fault.SyntaxFault{
-				Index: col + len(num) + 1,
-				Msgs: []string{
-					"Invalid number format, either...",
-					" - fractional digits are missing",
-					" - or the decimal separator is a typo",
-				},
-			}
-
-			return
-		}
-
-		num = append(num, frac...)
+	num, err = appendFractional(num, frac, col)
+	if err != nil {
+		out = nil
+		return
 	}
 
 	tk = &token.Token{
@@ -42,4 +28,32 @@ func scanNumber(in []rune, col int) (tk *token.Token, out []rune, err fault.Faul
 	}
 
 	return
+}
+
+// appendFractional appends the fractional part of the number.
+func appendFractional(num, frac []rune, col int) ([]rune, fault.Fault) {
+	size := len(frac)
+
+	if size > 0 {
+		if size < 2 {
+			return nil, badNumberFormat(col, len(num))
+		}
+
+		num = append(num, frac...)
+	}
+
+	return num, nil
+}
+
+// badNumberFormat returns a fault detailing a bad number
+// format.
+func badNumberFormat(col, offset int) fault.Fault {
+	return fault.SyntaxFault{
+		Index: col + offset + 1,
+		Msgs: []string{
+			"Invalid number format, either...",
+			" - fractional digits are missing",
+			" - or the decimal separator is a typo",
+		},
+	}
 }
