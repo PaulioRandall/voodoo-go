b213aa0d308966c429b73faac17bdafe795aef5f
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-08-20 22:28:05 +0100

    Rewrite scanSpell() + rewrite tests without test runner
    This is part of the initiative to rewrite the scanner in the
    manner suggested by Rob Pike https://talks.golang.org/2011/lex.slide#1.
    I found the test runner to be more of a hindrince than help when
    debugging, it's likely that other simplifications and changes have
    rendered it useless.

---
+++ parser/scanner_new/spell_test.go
@@ -0,0 +1,42 @@
+package scanner_new
+
+import (
+	"testing"
+
+	"github.com/PaulioRandall/voodoo-go/parser/token"
+)
+
+func doTestScanSpell(t *testing.T, in string, exp, expErr *token.Token) {
+	r := dummyRuner(in)
+	tk, _, errTk := scanSpell(r)
+	assertToken(t, exp, tk)
+	assertToken(t, expErr, errTk)
+}
+
+func dummySpellToken(end int, s string) token.Token {
+	return dummyToken(0, 0, end, s, token.TT_SPELL)
+}
+
+func TestScanSpell_1(t *testing.T) {
+	in := `@Println`
+	exp := dummySpellToken(8, `@Println`)
+	doTestScanSpell(t, in, &exp, nil)
+}
+
+func TestScanSpell_2(t *testing.T) {
+	in := `@a__12__xy__`
+	exp := dummySpellToken(12, `@a__12__xy__`)
+	doTestScanSpell(t, in, &exp, nil)
+}
+
+func TestScanSpell_3(t *testing.T) {
+	in := `@Println(msg)`
+	exp := dummySpellToken(8, `@Println`)
+	doTestScanSpell(t, in, &exp, nil)
+}
+
+func TestScanSpell_4(t *testing.T) {
+	in := `@2`
+	expErr := errDummyToken(0, 0, 2)
+	doTestScanSpell(t, in, nil, &expErr)
+}
